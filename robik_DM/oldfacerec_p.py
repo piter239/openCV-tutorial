b74dd5b16e4ad394c549196f03c7516e51966085
piter239 <piter239@gmail.com>
2019-12-28 22:14:39 +0300

    приветствия для Unknown
    выбросил неиспользуемые куски, добавил словари приветствий

--- robik_DM/oldfacerec_p.py
+++ robik_DM/oldfacerec_p.py
@@ -6,7 +6,8 @@ import glob
 import os
 import subprocess
 import pickle
-from pyimagesearch.centroidtracker import CentroidTracker 
+#from pyimagesearch.centroidtracker import CentroidTracker
+import random
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
@@ -20,8 +21,8 @@ from pyimagesearch.centroidtracker import CentroidTracker
 
 
 
-frame_width = 640  
-frame_height = 480 
+frame_width = 640
+frame_height = 480
 start = int(time.time())
 out = cv2.VideoWriter('./videos/neuroOut' + str(start) + '.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 def update_log_video():
@@ -32,7 +33,7 @@ def update_log_video():
 
 
 
-ct = CentroidTracker()
+#ct = CentroidTracker()
 video_capture = cv2.VideoCapture(0)
 video_capture.set(3, 640)
 video_capture.set(4, 480)
@@ -44,13 +45,14 @@ known_face_encodings = []
 
 known_face_names = []
 
+
 def getName(strs):
     if strs[0] == ".":
         strs = strs[1:]
     ret = ""
     for j in strs:
         if j == ".":
-            break;
+            break
         if j == "/":
             ret = ""
         else:
@@ -84,11 +86,35 @@ else:
         val = face_saves[key]
         known_face_encodings.append(val)
         known_face_names.append(key)
+
+# remember a list of greetings
+greetings = [
+    "Привет!",
+    "Меня зовут Робик",
+    "С наступающим ПРАЗДНИКОМ!",
+    "С Новым ГОДОМ!",
+    "Здравствуй, друг!",
+    "Приветствую!",
+    "Рад встрече",
+    "Привет, давай дружить! Я робот Робик",
+    "Привет, как тебя зовут"
+]
+
+
+# minimal times between greetings
 last_hello = dict()
 
+last_hello["all"] = 0   # global
+last_hello["Unknown"] = 0   # Unknown
 for i in known_face_names:
-    last_hello[i] = 0
-last_hello["Unknown"] = 0
+    last_hello[i] = 0   #
+
+# set minimal time between consequent "Hello!" for Unknown
+t_all = 5
+
+# set minimal time between consequent "I know you!" for the same name
+t = 20
+
 
 
 # Initialize some variables
@@ -97,10 +123,15 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
+
+#for future refactoring
+def DM_greet(str):
+    ret
+
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
-    #frame = frame[::-1, :, :]
+
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
@@ -108,7 +139,7 @@ while True:
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    
+
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
@@ -127,55 +158,67 @@ while True:
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
-            #print(matches)
 
             if face_distances[best_match_index] < 0.45 and matches[best_match_index]:
                 name = known_face_names[best_match_index]
-            face_dists.append(face_distances[best_match_index])    
+                print(name)
+            face_dists.append(face_distances[best_match_index])
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
     # Display the results
     rects = []
+
+# ПО УМУ ТУТ ПРОСТО НАДО ОТСОРТИРОВАТЬ ПО ДИСТАНЦИЯМ. Увы, моего знания Питона не хватает (Пётр)
+    found_known_face = False
     for (top, right, bottom, left), name, dist in zip(face_locations, face_names, face_dists):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+
+# review: я не понимаю, почему это работает при двух и более лицах в кадре
+# - мы ВНУТРИ цикла увеличиваем вдвое
         top *= 2
         right *= 2
         bottom *= 2
         left *= 2
         rects.append(np.array([right, top, left, bottom]))
+
+        print(time.time() - last_hello["all"])
+
         # Draw a box around the face
-        #cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
         cv2.circle(frame, ((left + right) // 2, (top + bottom) // 2), max(abs(left - right) // 2, abs(top - bottom) // 2) + 20, (255, 0, 0), 3)
-        # Draw a label with a name below the face
-        #cv2.rectangle(frame, (left, bottom), (max(left + len(name) * 17, right), bottom + 35), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        t = 30 * 60
-        t = 30
-        #cv2.putText(frame, name, (left + 6, bottom - 6 + 35), font, 1.0, (255, 255, 255), 1)
-        #print(dist)
-        if name != "Unknown":
-            if time.time() - last_hello[name] > t:
-                print("Привет " + getName(name))
+
+        if name != "Unknown" and not found_known_face:
+            found_known_face = True
+            last_hello["all"] = time.time() + t # pause with greetings no Unknown
+            (topN, rightN, bottomN, leftN) = (top, right, bottom, left)
+            if time.time() - last_hello[name] > t: # and time.time() - last_hello["all"] > t_all:
+                greeting = "Я думаю, ты " + getName(name) + ". Подскажи, угадал ли я."
+                print(greeting)
                 if not os.name == 'nt':
-                    subprocess.Popen(["echo Я думаю, ты " + getName(name) + ".  |RHVoice-test -p Elena"], shell=True)
-                
+                    subprocess.Popen(["echo  " + greeting + "  |RHVoice-test -p Elena"], shell=True)
+                last_hello["all"] = time.time() + t # pause with greetings no Unknown
                 last_hello[name] = time.time()
-        else:
+
+                # remember POI
+                found_known_face = True
+                (topN, rightN, bottomN, leftN) = (top, right, bottom, left)
+
+    for (top, right, bottom, left), name, dist in zip(face_locations, face_names, face_dists):
+
+        if not (name != "Unknown" and not found_known_face):
             print("detect " + name)
-            if time.time() - last_hello[name] > t*0.6371:
-                #cv2.circle(frame, ((left + right) // 2, (top + bottom) // 2), max(abs(left - right) // 2, abs(top - bottom) // 2) + 18, (0, 0, 255), 2)
-                print("Привет " + name)                
-                #engine.say("Hello " + name)
-                #engine.runAndWait()
-                #os.system("echo Привет " + name + " |RHVoice-test -p Elena")
+            if time.time() - last_hello["all"] > t_all:
+                greeting = random.choice(greetings)
+                print(greeting)
                 if not os.name == 'nt':
-                    subprocess.Popen(["echo Привет, друг! С наступающим Новым Годом!  |RHVoice-test -p Elena"], shell=True)                
+                    subprocess.Popen(["echo  " + greeting + "  |RHVoice-test -p Elena"], shell=True)
+                last_hello["all"] = time.time()
                 last_hello[name] = time.time()
-            
-        
-        
+
+    if found_known_face:
+        cv2.circle(frame, ((leftN + rightN) // 2, (topN + bottomN) // 2), max(abs(leftN - rightN) // 2, abs(topN - bottomN) // 2) + 20, (0, 255, 0), 4)
+
     out.write(frame)
     t2 = 5 * 60
     if time.time() - start > t2:
